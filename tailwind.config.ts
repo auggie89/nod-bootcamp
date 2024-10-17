8a0afb838fc2619879d202578de5a3160574dead
Ognjen Umicevic <ognjenumicevic89@gmail.com>
2024-10-17 20:58:29 +0200

    add tailwind, storybook and astro

---
+++ tailwind.config.ts
@@ -0,0 +1,13 @@
+import type { Config } from 'tailwindcss'
+
+const config: Config = {
+  content: [
+    './src/**/*.{astro,html,js,jsx,ts,tsx}'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+export default config
